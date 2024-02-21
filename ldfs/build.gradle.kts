b207594ff479b3c75f148099ba365815cebe4123
space-enthusiast <jinding0829@gmail.com>
2024-02-21 11:17:17 +0900

    feat: initial spring boot project

---
+++ ldfs/build.gradle.kts
@@ -0,0 +1,39 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    val kotlinVersion = "1.9.22"
+    val springBootVersion = "3.2.2"
+    id("org.springframework.boot") version springBootVersion
+    id("io.spring.dependency-management") version "1.1.4"
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.spring") version kotlinVersion
+}
+
+group = "com"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "21"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ ldfs/src/test/kotlin/com/ldfs/LdfsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ldfs
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LdfsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
