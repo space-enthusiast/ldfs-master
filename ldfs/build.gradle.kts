7416186395fff0366934790c40e0031fdaa7de73
space-enthusiast <jinding0829@gmail.com>
2024-03-26 08:48:58 +0900

    feat: in memory h2 database set

--- ldfs/build.gradle.kts
+++ ldfs/build.gradle.kts
@@ -23,6 +23,8 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("com.h2database:h2")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
