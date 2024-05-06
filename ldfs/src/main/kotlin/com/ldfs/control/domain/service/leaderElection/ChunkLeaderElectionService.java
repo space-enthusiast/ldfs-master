eafb11c61a5ab78f91053e6be5e200e80dc857d4
KimSeungHyun <kimseunghyun@KimSeungHyunui-MacBookPro.local>
2024-05-07 03:48:51 +0900

    hotfix done

--- ldfs/src/main/kotlin/com/ldfs/control/domain/service/leaderElection/ChunkLeaderElectionService.java
+++ ldfs/src/main/kotlin/com/ldfs/control/domain/service/leaderElection/ChunkLeaderElectionService.java
@@ -23,16 +23,13 @@ import java.util.concurrent.*;
 public class ChunkLeaderElectionService {
     private final ChunkServerAccessService chunkServerAccessService;
     private final LeaderElectionRequestService leaderElectionRequestService;
-    private final ExecutorService executorService;
-    private final Map<String, CompletableFuture<ResponseEntity>> ongoingElections;
-    private final int TIMEOUT_DURATION = 5000;
+    private final ConcurrentHashMap<String, Pair<String, Pair<InetSocketAddress, UUID>>> requestState = new ConcurrentHashMap<>();
 
     @Autowired
-    public ChunkLeaderElectionService(ChunkServerAccessService chunkServerAccessService, LeaderElectionRequestService leaderElectionRequestService, ExecutorService executorService, Map<String, CompletableFuture<ResponseEntity>> ongoingElections) {
+    public ChunkLeaderElectionService(ChunkServerAccessService chunkServerAccessService,
+                                      LeaderElectionRequestService leaderElectionRequestService) {
         this.chunkServerAccessService = chunkServerAccessService;
         this.leaderElectionRequestService = leaderElectionRequestService;
-        this.executorService = executorService;
-        this.ongoingElections = ongoingElections;
     }
 
     // ------------------------------------------------------------
@@ -42,32 +39,40 @@ public class ChunkLeaderElectionService {
     */
     private final String successState = "SUCCESS";
     private final String waitingState = "WAITING";
-    private final ConcurrentHashMap<String, Pair<String, Pair<InetSocketAddress, UUID>>> requestState = new ConcurrentHashMap<>();
 
-//    initiate leader election
-    public void sendAsyncLeaderElectionRequest(List<ChunkServerEntity> broadCastList) {
+
+    //    initiate leader election
+    public String sendAsyncLeaderElectionRequest(List<ChunkServerEntity> broadCastList) {
         String requestCheckSum = generateChecksum(broadCastList);
         for (int i = 0; i < broadCastList.size(); i++) {
             leaderElectionRequestService.httpLeaderElectionService(broadCastList.get(i), broadCastList, requestCheckSum);
         }
+        return requestCheckSum;
     }
 
-//    1, sendAsyncLeaderElectionRequest
+    //    1, sendAsyncLeaderElectionRequest
 //    2, getLeaderElectedChunkServer
-    public Pair<InetSocketAddress, UUID> getLeaderElectedChunkServer() {
-        // 1초간 100 ms주기로 확인하다가
-//        try {
-//            ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor()
-//            ScheduledFuture<Integer> future = executor.scheduleAtFixedRate(
-//                () -> 1,
-//                1,
-//                TimeUnit.SECONDS
-//            );
-//
-//        } catch (InterruptedException | ExecutionException e) {
-//            Thread.currentThread().interrupt();
-//            return null;
-//        }
+    public Pair<InetSocketAddress, UUID> getLeaderElectedChunkServer(String requestCheckSum) {
+        CompletableFuture<Pair<InetSocketAddress, UUID>> future = new CompletableFuture<>();
+
+        ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+        executor.scheduleAtFixedRate(() -> {
+            if (requestState.containsKey(requestCheckSum) && requestState.get(requestCheckSum).getFirst().equals(successState)) {
+                future.complete(requestState.get(requestCheckSum).getSecond()); // Complete the future when result is found
+            }
+        }, 0, 1, TimeUnit.SECONDS); // Initial delay of 0, execute every 1 second
+
+        // Optionally, you can cancel the future after a certain duration
+        executor.schedule(() -> future.cancel(false), 10, TimeUnit.SECONDS); // Cancel after 10 seconds
+
+        try {
+            return future.get(); // Wait for the future to complete and return the result
+        } catch (InterruptedException | ExecutionException e) {
+            Thread.currentThread().interrupt();
+            return null;
+        } finally {
+            executor.shutdown(); // Shut down the executor
+        }
     }
 
     // api that the chunk server uses to declare itself the leader
@@ -84,11 +89,12 @@ public class ChunkLeaderElectionService {
         List<ChunkServerEntity> broadCastList = candidates.stream().map(chunkServerAccessService::findServerWithSpecificChunk).toList();
         try {
             leaderElectionChunkStateLock(candidates);
-            leaderElectionHTTPRequest(broadCastList);
+            String checksum = sendAsyncLeaderElectionRequest(broadCastList);
+            getLeaderElectedChunkServer(checksum);
+
         } catch (Exception e) {
 
         }
-//        return new LeaderFollowerChunkServers(leader, followers);
     }
 
     private static void leaderElectionChunkStateLock(List<ChunkEntity> candidates) {
@@ -96,9 +102,6 @@ public class ChunkLeaderElectionService {
             candidate.setStateChunk(ChunkState.ELECTING);
         }
     }
-
-
-
 
 
     public LeaderFollowerChunkServers mockElectLeader(List<ChunkEntity> candidates) {
@@ -124,25 +127,5 @@ public class ChunkLeaderElectionService {
             // Handle exception
             return null;
         }
-    }
-
-    //    isResponseReceived: Checks if a response for a particular checksum is ongoing,
-    public boolean isElectionOngoing(String checksum) {
-        return ongoingElections.containsKey(checksum);
-    }
-    //    retrieves the CompletableFuture associated with a given checksum,
-//    allowing external components to track the progress or outcome of the leader election.
-    public CompletableFuture<ResponseEntity> getElectionFuture(String checksum) {
-        return ongoingElections.get(checksum);
-    }
-
-    //    completeElection: FORCE-Completes the leader election process for a given checksum by providing the corresponding response.
-//    It removes the checksum from the map if it's still present and the associated CompletableFuture is not already completed.
-    public CompletableFuture<ResponseEntity> forceCompleteElection(String checksum, ResponseEntity<ChunkEntity> response) {
-        CompletableFuture<ResponseEntity> future = ongoingElections.remove(checksum);
-        if (future != null && !future.isDone()) {
-            future.complete(response);
-        }
-        return future;
     }
 }
--- ldfs/src/main/kotlin/com/ldfs/control/domain/service/leaderElection/LeaderElectionServiceConfig.java
+++ ldfs/src/main/kotlin/com/ldfs/control/domain/service/leaderElection/LeaderElectionServiceConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.concurrent.ExecutorService;
+
 @Configuration
 public class LeaderElectionServiceConfig {
     @Bean
