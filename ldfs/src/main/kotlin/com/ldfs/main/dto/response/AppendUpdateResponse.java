fe8900f01a3d0804b21938575b280ce23639473e
KimSeungHyun <kimseunghyun@192.168.nate.com>
2024-05-11 16:54:36 +0900

    recovered

--- ldfs/src/main/kotlin/com/ldfs/main/dto/response/AppendUpdateResponse.java
+++ ldfs/src/main/kotlin/com/ldfs/main/dto/response/AppendUpdateResponse.java
@@ -1,7 +1,6 @@
 package com.ldfs.main.dto.response;
 
 import com.ldfs.control.domain.model.entity.ChunkServerEntity;
-import com.ldfs.main.dto.LeaderFollowerChunkServers;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
@@ -11,16 +10,10 @@ import java.util.List;
 @Data
 public class AppendUpdateResponse {
     List<ChunkServerEntity> nonleasedServer;
-
     ChunkServerEntity leasedServer;
 
     public AppendUpdateResponse(ChunkServerEntity leasedServer, List<ChunkServerEntity> nonleasedServer) {
         this.nonleasedServer = nonleasedServer;
         this.leasedServer = leasedServer;
-    }
-
-    public AppendUpdateResponse(LeaderFollowerChunkServers record) {
-        this.leasedServer = record.leased();
-        this.nonleasedServer = record.nonLeased();
     }
 }
