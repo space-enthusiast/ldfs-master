bde038cf7c30813627779f0a6e4af003de2490d9
Kim-Jung <jung.kim@voithru.com>
2024-05-25 17:22:47 +0900

    fix: rest controller code was inside constructor

--- ldfs/src/main/kotlin/com/ldfs/main/ChunkServerController.kt
+++ ldfs/src/main/kotlin/com/ldfs/main/ChunkServerController.kt
@@ -1,7 +1,6 @@
 import com.ldfs.control.domain.model.entity.ChunkServerEntity
 import com.ldfs.control.domain.service.ChunkServerAccessService
 import com.ldfs.main.dto.request.HeartBeatRequest
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
@@ -11,24 +10,24 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/chunkServer")
-class ChunkServerController
-    @Autowired
-    constructor(private val chunkServerAccessService: ChunkServerAccessService) {
-        @GetMapping
-        fun chunkServers(): ResponseEntity<List<ChunkServerEntity>> {
-            return ResponseEntity.ok().body(chunkServerAccessService.findAll())
-        }
-
-        @PostMapping("/heartBeat")
-        fun heartBeatUpdateServerStatus(
-            @RequestBody heartBeatRequest: HeartBeatRequest,
-        ): ResponseEntity<ChunkServerEntity> {
-            val storedEntity =
-                chunkServerAccessService.makeServerDiscoverable(
-                    heartBeatRequest.ip!!,
-                    heartBeatRequest.port!!,
-                    heartBeatRequest.remainingStorageSize!!,
-                )
-            return ResponseEntity.ok().body(storedEntity)
-        }
+class ChunkServerController(
+    private val chunkServerAccessService: ChunkServerAccessService,
+) {
+    @GetMapping
+    fun chunkServers(): ResponseEntity<List<ChunkServerEntity>> {
+        return ResponseEntity.ok().body(chunkServerAccessService.findAll())
     }
+
+    @PostMapping("/heartBeat")
+    fun heartBeatUpdateServerStatus(
+        @RequestBody heartBeatRequest: HeartBeatRequest,
+    ): ResponseEntity<ChunkServerEntity> {
+        val storedEntity =
+            chunkServerAccessService.makeServerDiscoverable(
+                heartBeatRequest.ip!!,
+                heartBeatRequest.port!!,
+                heartBeatRequest.remainingStorageSize!!,
+            )
+        return ResponseEntity.ok().body(storedEntity)
+    }
+}
